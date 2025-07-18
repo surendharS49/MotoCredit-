02670610f148029eb90e6665f55f429dc75ad1c3
surendhar <128160322+surendharS49@users.noreply.github.com>
2025-07-18 21:23:48 +0530

    new url update

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // More detailed CORS configuration
 app.use(cors({
-  origin: 'http://localhost:5173', // Replace with your frontend URL
+  origin: ['http://localhost:5173', 'https://motocredit.onrender.com'], // Replace with your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
